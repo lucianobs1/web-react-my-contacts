d047d5043bae53d4f0eb84a7011ad3e0a27a3090
lucianobs1 <lucianotetsu@gmail.com>
2023-08-15 18:42:53 -0300

    add components select and input

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,19 @@
 import { PageHeader } from "../../components/PageHeader";
+import { Select } from "../../components/Select";
+import { Input } from "../../components/input";
 
 export function NewContact() {
   return (
-    <div>
+    <>
       <PageHeader title="Novo contato" />
-    </div>
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="1">Instagram</option>
+        <option value="2">Facebook</option>
+        <option value="3">TikTok</option>
+        <option value="4">ALura</option>
+      </Select>
+    </>
   );
 }
