6df9d00cea162076d2e5267e79fa10ec36c4ad65
lucianobs1 <lucianotetsu@gmail.com>
2023-08-16 10:28:06 -0300

    add component button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import { Button } from "../../components/Button";
 import { PageHeader } from "../../components/PageHeader";
 import { Select } from "../../components/Select";
 import { Input } from "../../components/input";
@@ -12,8 +13,14 @@ export function NewContact() {
         <option value="1">Instagram</option>
         <option value="2">Facebook</option>
         <option value="3">TikTok</option>
-        <option value="4">ALura</option>
+        <option value="4">Alura </option>
       </Select>
+
+      <Button type="button">Salvar alterações</Button>
+
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
     </>
   );
 }
